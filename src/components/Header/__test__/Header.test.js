9781daf8090d31b8911681f25e3578c4e63a0c25
hampus <hampus.john.olausson@gmail.com>
2021-11-04 18:10:37 +0100

    Test that checks how many incomplete tasks there is

--- src/components/Header/__test__/Header.test.js
+++ src/components/Header/__test__/Header.test.js
@@ -9,38 +9,38 @@ it('should render same text passed into prop', async () => {
   expect(headingElement).toBeInTheDocument();
 });
 
-it('should render same text passed into sprop', async () => {
-  render(<Header title="my header" />);
-  const headingElement = screen.getByRole("heading", {name: "My header"});
-  expect(headingElement).toBeInTheDocument();
-});
+// it('should render same text passed into sprop', async () => {
+//   render(<Header title="my header" />);
+//   const headingElement = screen.getByRole("heading", {name: "My header"});
+//   expect(headingElement).toBeInTheDocument();
+// });
 
-it('should render same text passed into titleprop', async () => {
-  render(<Header title="my header" />);
-  const headingElement = screen.getByTitle("Header");
-  expect(headingElement).toBeInTheDocument();
-});
+// it('should render same text passed into titleprop', async () => {
+//   render(<Header title="my header" />);
+//   const headingElement = screen.getByTitle("Header");
+//   expect(headingElement).toBeInTheDocument();
+// });
 
-it('should render same text passed into idprop', async () => {
-  render(<Header title="my header" />);
-  const headingElement = screen.getByTestId("header-2");
-  expect(headingElement).toBeInTheDocument();
-});
+// it('should render same text passed into idprop', async () => {
+//   render(<Header title="my header" />);
+//   const headingElement = screen.getByTestId("header-2");
+//   expect(headingElement).toBeInTheDocument();
+// });
 
-it('should render same text passed into findbyprop', async () => {
-  render(<Header title="my header" />);
-  const headingElement = await screen.findByText(/my header/i);
-  expect(headingElement).toBeInTheDocument();
-});
+// it('should render same text passed into findbyprop', async () => {
+//   render(<Header title="my header" />);
+//   const headingElement = await screen.findByText(/my header/i);
+//   expect(headingElement).toBeInTheDocument();
+// });
 
-it('should render same text passed into queryfindbyprop', async () => {
-  render(<Header title="my header" />);
-  const headingElement = screen.queryby(/dogs/i);
-  expect(headingElement).not.toBeInTheDocument();
-});
+// it('should render same text passed into queryfindbyprop', async () => {
+//   render(<Header title="my header" />);
+//   const headingElement = screen.queryby(/dogs/i);
+//   expect(headingElement).not.toBeInTheDocument();
+// });
 
-it('should render same text passed into getallby role', async () => {
-  render(<Header title="my header" />);
-  const headingElements = screen.getAllByRole("heading");
-  expect(headingElements.length).toBe(2);
-});
+// it('should render same text passed into getallby role', async () => {
+//   render(<Header title="my header" />);
+//   const headingElements = screen.getAllByRole("heading");
+//   expect(headingElements.length).toBe(2);
+// });
